427aec10353e7112eaeeb11b15629f2daf502858
Juan Muniz <juan@leao.com>
2025-03-13 16:27:13 -0300

    Projeto campsite bio versão1

---
+++ src/components/Campsite.js
@@ -0,0 +1,34 @@
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import styles from './Campsite.module.css';
+
+function Campsite() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.animatedLetters}>JM</div>
+
+            <h1 className={styles.title}>Bem-vindo ao meu Campsite!</h1>
+            <ul className={styles.linkList}>
+                <li>
+                    <a href="https://github.com/Jotaeme00" className={styles.link}>
+                        <FaGithub className={styles.icon} /> Github
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/juan-muniz-b695132b6/" className={styles.link}>
+                        <FaLinkedin className={styles.icon} /> Linkedin
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/j0t4eme_" className={styles.link}>
+                        <FaInstagram className={styles.icon} /> Instagram 
+                        <span className={styles.pessoal}>(pessoal)</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Campsite;
+
+
