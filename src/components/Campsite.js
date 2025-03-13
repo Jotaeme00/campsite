da31722faa5a0a1f75a3b57293ec66d07afb0656
Juan Muniz <juan@leao.com>
2025-03-13 18:16:34 -0300

    Campsite atualizado(novas animações) 2versão

--- src/components/Campsite.js
+++ src/components/Campsite.js
@@ -2,6 +2,8 @@ import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import styles from './Campsite.module.css';
 
 function Campsite() {
+    
+
     return (
         <div className={styles.container}>
             <div className={styles.animatedLetters}>JM</div>
@@ -10,24 +12,24 @@ function Campsite() {
             <ul className={styles.linkList}>
                 <li>
                     <a href="https://github.com/Jotaeme00" className={styles.link}>
-                        <FaGithub className={styles.icon} /> Github
+                        <FaGithub className={styles.icon} aria-label="Github" /> Github
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/juan-muniz-b695132b6/" className={styles.link}>
-                        <FaLinkedin className={styles.icon} /> Linkedin
+                        <FaLinkedin className={styles.icon} aria-label="Linkedin" />Juan - Linkedin
                     </a>
                 </li>
                 <li>
                     <a href="https://www.instagram.com/j0t4eme_" className={styles.link}>
-                        <FaInstagram className={styles.icon} /> Instagram 
-                        <span className={styles.pessoal}>(pessoal)</span>
+                        <FaInstagram className={styles.icon} aria-label="Instagram" /> Instagram 
+                        <span className={styles.pessoal}>PRIVADO</span>
                     </a>
                 </li>
             </ul>
         </div>
     );
-}
+};
 
 export default Campsite;
 
